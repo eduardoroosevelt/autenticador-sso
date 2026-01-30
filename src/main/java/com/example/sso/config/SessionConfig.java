c2352a7b572c90148ae1fe4a24c8bebe3d896a4c
eduardoroosevelt <eduardo.roosevelt.de.oliveira@gmail.com>
2026-01-29 23:40:30 -0400

    Fix RedisIndexedSessionRepository wiring

--- src/main/java/com/example/sso/config/SessionConfig.java
+++ src/main/java/com/example/sso/config/SessionConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
@@ -21,6 +22,13 @@ public class SessionConfig {
 
   @Bean
   public RedisIndexedSessionRepository sessionRepository(RedisConnectionFactory connectionFactory) {
-    return new RedisIndexedSessionRepository(connectionFactory);
+    return new RedisIndexedSessionRepository(sessionRedisTemplate(connectionFactory));
+  }
+
+  @Bean
+  public RedisTemplate<String, Object> sessionRedisTemplate(RedisConnectionFactory connectionFactory) {
+    RedisTemplate<String, Object> template = new RedisTemplate<>();
+    template.setConnectionFactory(connectionFactory);
+    return template;
   }
 }
